f6e15428def8b1521cb457a4e033e0fab88990a4
Andres Fernandez-Murray <andres.fmurray@gmail.com>
2021-06-02 17:15:37 +0000

    cleaned up play.js, exporting input and client modules

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
-const connect = require("./client");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (data) {
-  if (data === "\u0003") {
-    process.exit();
-  }
-};
 
 setupInput();
