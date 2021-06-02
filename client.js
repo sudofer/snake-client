d8da60a2aad31c4b5a7c7ad5b09549c76f162431
Andres Fernandez-Murray <andres.fmurray@gmail.com>
2021-06-02 16:45:28 +0000

    seperated connection and constants

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require("net");
+const { host, port } = require("./constants");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host, // IP address here,
+    port, // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("connected!!");
+  });
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
---
+++ constants.js
@@ -0,0 +1,4 @@
+const host = "135.23.222.131";
+const port = 50542;
+
+module.exports = { host, port };
